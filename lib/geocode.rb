9af9334a38a6050d38b8b253fd012011a8c14d71
Rahul Trikha <rahul.trikha@gmail.com>
2013-06-01 20:03:11 +0930

    Updated adult education csv with geo coordinates

--- lib/geocode.rb
+++ lib/geocode.rb
@@ -3,7 +3,11 @@
 require 'rubygems'
 require 'geocoder'
 require 'csv'
-require 'pry'
+
+if ARGV.empty? || ARGV.size < 2
+  puts "Usage ./geocode input_file output_file"
+  exit
+end
 
 def find_geolocation(address)
   geolocation = {}
@@ -15,18 +19,23 @@ def find_geolocation(address)
     geolocation[:latitude] = result[0].latitude
     geolocation[:longitude] = result[0].longitude
   end
-  
+
   return geolocation
 end
 
-CSV.open("communities_updated.csv", "wb") do |write|
-  CSV.open("communities.csv", "r", headers: :first_row, header_converters: :symbol) do |read|
+current_directory = Dir.pwd
+input_file  = File.join(current_directory, ARGV[0])
+output_file = File.join(current_directory, ARGV[1])
+
+CSV.open(output_file, "wb") do |write|
+  CSV.open(input_file, "r", headers: :first_row, header_converters: :symbol) do |read|
     write << read.first.headers
     read.find_all do |row|
       geolocation = find_geolocation(row[:address])
       row[:latitude]  = geolocation[:latitude]
       row[:longitude] = geolocation[:longitude]
       write << row
+      puts "#{row[:address]} -> { #{row[:latitude]}, #{row[:longitude]} }"
     end
   end
 end
