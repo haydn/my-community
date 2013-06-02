8027fb84126229754ee308a70dba292095ca87b6
Rahul Trikha <rahul.trikha@gmail.com>
2013-06-02 11:52:01 +0930

    Added basic get involved script

---
+++ javascripts/get-involved/mongolab.js
@@ -0,0 +1,23 @@
+// This is a module for cloud persistance in mongolab - https://mongolab.com
+angular.module('mongolab', ['ngResource']).
+    factory('Project', function($resource) {
+      var Project = $resource('https://api.mongolab.com/api/1/databases' +
+          '/my-community/collections/projects/:id',
+          { apiKey: 'AF-jDz3QvTMrBRbAh09TS85kN8hLgg6T' }, {
+            update: { method: 'PUT' }
+          }
+      );
+
+      Project.prototype.update = function(cb) {
+        return Project.update({id: this._id.$oid},
+            angular.extend({}, this, {_id:undefined}), cb);
+      };
+
+      Project.prototype.destroy = function(cb) {
+        return Project.remove({id: this._id.$oid}, cb);
+      };
+
+      return Project;
+    });
+
+    // https://api.mongolab.com/api/1/databases/my-community/collections?apiKey=AF-jDz3QvTMrBRbAh09TS85kN8hLgg6T
